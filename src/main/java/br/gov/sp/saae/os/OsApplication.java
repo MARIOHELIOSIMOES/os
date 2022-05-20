c4a6b35179285fb656500d97674c661be3a9b1bb
Mário Hélio Simões <marioheliosimoes@saaemogi.com.br>
2022-05-20 16:48:53 -0300

    commit inicial

---
+++ src/main/java/br/gov/sp/saae/os/OsApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.saae.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsApplication.class, args);
+	}
+
+}
